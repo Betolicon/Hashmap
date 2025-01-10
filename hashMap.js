3208a45720c2d05618babc3ba0c88c555af20d28
Betolicon <[albertlicontti@gmail.com]>
2025-01-10 19:38:14 -0400

    I created the files I am going to be using

---
+++ hashMap.js
@@ -0,0 +1,11 @@
+// class Greetings {
+//     constructor(name){
+//         this.name = name
+//     }
+
+//     saludar(){
+//         return `Hello, ${this.name}`
+//     }
+// } 
+
+// module.exports = { Greetings };
---
+++ main.mjs
@@ -0,0 +1,5 @@
+// import pkg from './hashMap.js'
+// const { Greetings } = pkg
+
+// const persona1 = new Greetings('Juan', 30);
+// console.log(persona1.saludar());
